0ca51efa598fbbe657ac83f940e39f9049bdd631
ChrisMP01 <Moquillaza98@gmail.com>
2021-10-08 17:40:14 +0200

    commit inicial

---
+++ src/main/java/com/christopher/quizProyecto/ControladorQuiz1.java
@@ -0,0 +1,9 @@
+package com.christopher.quizProyecto;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ControladorQuiz1 {
+	//@RequestParam(value="",method=RequestMethod.POST)
+	//public String
+}
---
+++ src/test/java/com/christopher/quizProyecto/QuizProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.christopher.quizProyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
